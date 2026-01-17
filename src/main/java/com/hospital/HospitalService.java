ff47030d9f0fe8c96d1cf71a356097e382214c9b
Ahmed7749 <ahmadkmm2@gmail.com>
2026-01-17 20:37:41 +0300

    reused the hospitalmanagement class from V2.0 put changed it's name to hospitalService

---
+++ src/main/java/com/hospital/HospitalService.java
@@ -0,0 +1,124 @@
+package com.hospital;
+
+import com.hospital.daos.AppointmentDAO;
+import com.hospital.daos.DoctorDAO;
+import com.hospital.daos.PatientDAO;
+import com.hospital.pojos.Appointment;
+import com.hospital.pojos.Doctor;
+import com.hospital.pojos.Patient;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+public class HospitalService {
+    private final PatientDAO patientDAO;
+    private final DoctorDAO doctorDAO;
+    private final AppointmentDAO appointmentDAO;
+    public HospitalService(PatientDAO patientDAO, DoctorDAO doctorDAO, AppointmentDAO appointmentDAO) {
+        this.patientDAO = patientDAO;
+        this.doctorDAO = doctorDAO;
+        this.appointmentDAO = appointmentDAO;
+    }
+
+    public boolean admitPatient(Patient patient){
+        Optional<Patient> patientOptional = patientDAO.findPatientByName(patient.getName());
+        if(patientOptional.isPresent()){
+            return false;
+        }
+        return patientDAO.addPatientToDB(patient);
+    }
+
+
+
+    public boolean updatePatientName(int patientId, String newName){
+        Optional<Patient> foundPatient = patientDAO.getPatientById(patientId);
+        return foundPatient.filter(patient -> patientDAO.updatePatientName(patient, newName)).isPresent();
+    }
+
+    public boolean deletePatientById(int id){
+        Optional<Patient> foundPatient = patientDAO.getPatientById(id);
+        return foundPatient.filter(patientDAO::deletePatientById).isPresent();
+    }
+
+
+    public List<Patient> getPatientsList(){
+        return patientDAO.getPatientsList();
+    }
+
+    public boolean addDoctor(Doctor doctor){
+        Optional<Doctor> foundDoctor = doctorDAO.getDoctorByName(doctor.getName());
+        if(foundDoctor.isPresent()){
+            return false;
+        }
+        return doctorDAO.addDoctorToDB(doctor);
+    }
+
+
+    public boolean updateDoctorName(int doctorId, String newName){
+        Optional<Doctor> foundDoctor = doctorDAO.getDoctorById(doctorId);
+        return foundDoctor.filter(doctor -> doctorDAO.updateDoctorName(doctor, newName)).isPresent();
+    }
+
+    public boolean deleteDoctorById(int id){
+        Optional<Doctor> doctor = doctorDAO.getDoctorById(id);
+        if(doctor.isPresent()){
+            return doctorDAO.deleteDoctorById(id);
+        }
+        return false;
+    }
+
+
+    public List<Doctor> getDoctorsList(){
+        return doctorDAO.getListOfDoctors();
+    }
+
+
+    public boolean bookAppointment(Appointment appointment){
+        if (appointmentDAO.isBooked(appointment.getAppointmentTime(), appointment.getAppointmentDate(), appointment.getPatient_id(), appointment.getDoctor_id())) {
+            return false;
+        }
+        return appointmentDAO.bookAppointment(appointment);
+    }
+
+    public boolean updateAppointmentTime(int appointmentId, LocalTime newTime){
+        Optional<Appointment> appointment = appointmentDAO.getAppointmentById(appointmentId);
+        if(appointment.isPresent()) {
+            if (appointmentDAO.isBooked(appointment.get().getAppointmentTime(), appointment.get().getAppointmentDate(), appointment.get().getPatient_id(), appointment.get().getDoctor_id())) {
+                return appointmentDAO.updateAppointmentTime(appointment.get(), newTime);
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public boolean updateAppointmentDate(int appointmentId, LocalDate newDate){
+        Optional<Appointment> appointment = appointmentDAO.getAppointmentById(appointmentId);
+        if(appointment.isPresent()) {
+            if (appointmentDAO.isBooked(appointment.get().getAppointmentTime(), appointment.get().getAppointmentDate(), appointment.get().getPatient_id(), appointment.get().getDoctor_id())) {
+                return appointmentDAO.updateAppointmentDate(appointment.get(), newDate);
+            }
+            return false;
+        }
+        return false;
+    }
+
+
+    public boolean deleteAppointmentById(int appointmentId){
+        Optional<Appointment> foundAppointment = appointmentDAO.getAppointmentById(appointmentId);
+        return foundAppointment.filter(appointmentDAO::deleteAppointment).isPresent();
+    }
+
+    public List<Appointment> getAppointmentList(){
+        return appointmentDAO.getListOfAppointments();
+    }
+
+    public List<Appointment> getAppointmentsForPatient(int patient_id){
+        return appointmentDAO.getAppointmentsByPatientId(patient_id);
+    }
+
+
+    public List<Doctor> getDoctorsByMajor(String major){
+        return doctorDAO.getDoctorsByMajor(major);
+    }
+}
