4e1f376635baf58e12d9d951ab832aff39b465fd
Ahmed7749 <ahmadkmm2@gmail.com>
2026-01-17 22:41:49 +0300

    added a new method so we can know which doctor or patient is assigned to which user

--- src/main/java/com/hospital/daos/GenericDAO.java
+++ src/main/java/com/hospital/daos/GenericDAO.java
@@ -3,15 +3,41 @@ package com.hospital.daos;
 import com.hospital.utils.DatabaseConnector;
 import com.hospital.utils.RowMapper;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class GenericDAO{
+
+    protected int executeUpdateReturnId(String sql, Object... parameters){
+        Connection con = null;
+        try{
+            con = DatabaseConnector.getDatabaseConnection();
+            try(PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+                for(int i = 0; i < parameters.length; i++){
+                    stmt.setObject(i + 1, parameters[i]);
+                }
+
+                stmt.executeUpdate();
+
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        return generatedKeys.getInt(1);
+                    }
+                }
+            }
+            return -1;
+        } catch(SQLException e){
+            throw new RuntimeException("Error in db", e);
+        } finally {
+            DatabaseConnector.returnConnection(con);
+        }
+    }
+
+
+
+
     protected boolean executeUpdate(String sql, Object... parameters){
         Connection con = null;
         try{
