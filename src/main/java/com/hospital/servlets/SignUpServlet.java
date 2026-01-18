aa2a3cae2a250bb60748cc9562b09c93a61f98d5
Ahmed7749 <ahmadkmm2@gmail.com>
2026-01-18 21:23:00 +0300

    added the request dispatcher

--- src/main/java/com/hospital/servlets/SignUpServlet.java
+++ src/main/java/com/hospital/servlets/SignUpServlet.java
@@ -28,6 +28,11 @@ public class SignUpServlet extends HttpServlet {
     }
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("signup.jsp").forward(req,resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
