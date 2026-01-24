68c29a8d3ead27c783369e053abe3afefd283e6b
Ahmed7749 <ahmadkmm2@gmail.com>
2026-01-24 17:02:25 +0300

    the manage doctor servlet and implementation

---
+++ src/main/java/com/hospital/servlets/Admin/ManageDoctorsServlet.java
@@ -0,0 +1,30 @@
+package com.hospital.servlets.Admin;
+
+import com.hospital.daos.DoctorDAO;
+import com.hospital.pojos.Doctor;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/doctors")
+public class ManageDoctorsServlet extends HttpServlet {
+    private DoctorDAO doctorDAO;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        doctorDAO = new DoctorDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Doctor> doctorList = doctorDAO.getListOfDoctors();
+        req.setAttribute("doctorList", doctorList);
+        req.getRequestDispatcher("/admin/doctors.jsp").forward(req,resp);
+    }
+}
