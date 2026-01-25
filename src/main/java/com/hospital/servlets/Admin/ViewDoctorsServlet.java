264d6316df9c85bda6e20cf02515a4e65dbb104a
Ahmed7749 <ahmadkmm2@gmail.com>
2026-01-25 08:05:05 +0300

    implemented a way of fetching the user object so we can access the username and password for it

--- src/main/java/com/hospital/servlets/Admin/ViewDoctorsServlet.java
+++ src/main/java/com/hospital/servlets/Admin/ViewDoctorsServlet.java
@@ -1,7 +1,9 @@
 package com.hospital.servlets.Admin;
 
 import com.hospital.daos.DoctorDAO;
+import com.hospital.daos.UserDAO;
 import com.hospital.pojos.Doctor;
+import com.hospital.pojos.User;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,26 +12,41 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @WebServlet("/admin/doctors")
 public class ViewDoctorsServlet extends HttpServlet {
     private DoctorDAO doctorDAO;
+    private UserDAO userDAO;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         doctorDAO = new DoctorDAO();
+        userDAO = new UserDAO();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Doctor> doctorList = doctorDAO.getListOfDoctors();
-        if(doctorList.isEmpty()){
-            req.setAttribute("error", "There are no doctors ): ");
-            req.getRequestDispatcher("/admin/doctors.jsp").forward(req,resp);
-            return;
+        Map<Integer, User> userMap = new HashMap<>();
+
+        if (doctorList != null && !doctorList.isEmpty()) {
+            for (Doctor doc : doctorList) {
+                int userId = doc.getUserId();
+                if (userId > 0) {
+                    Optional<User> userOpt = userDAO.getUserById(userId);
+                    userOpt.ifPresent(user -> userMap.put(doc.getId(), user));
+                }
+            }
+        } else {
+            req.setAttribute("error", "There are no doctors registered.");
         }
+
         req.setAttribute("doctorList", doctorList);
-        req.getRequestDispatcher("/admin/doctors.jsp").forward(req,resp);
+        req.setAttribute("userMap", userMap);
+        req.getRequestDispatcher("/admin/doctors.jsp").forward(req, resp);
     }
 }
