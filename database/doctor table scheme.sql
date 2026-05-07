7983596966dbc5e94d2103d4fea2dfdbf94f4345
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-07 20:29:18 +0300

    Deleted old schemes

--- database/doctor table scheme.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE doctors(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    major ENUM('SURGEON', 'GENERAL') NOT NULL,
-    gender ENUM('FEMALE','MALE') NOT NULL,
-	user_id INT NOT NULL
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
